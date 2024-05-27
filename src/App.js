28a2e71396018f8554da4e67af1232438cbd1d8b
Geomorpholog <Geomorpholog@rambler.ru>
2024-05-27 10:04:37 +0300

    SWR add

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import BarChat from './barChat.js';
-import {useRef} from "react";
-
-
 
 export default function App(props){
 
-   const dataset = useRef();
-
-    document.addEventListener("DOMContentLoaded",
-    
-    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-    .then((response) => {
-      return response.json();
-   })
-    .then((data) => {
-       dataset.current =  JSON.stringify(data["data"])
-              }
-   ));
-
-  
 return(
       <div id = "background">
         <div id ="app">
             <h4>United States GDP</h4>
            <BarChat 
-           data = {dataset.current} 
-           
+           URL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
            />
         </div>
       </div>
 )
     
-    
-   
-
-    
-
 }
--- src/barChat.js
+++ src/barChat.js
@@ -1,21 +1,37 @@
 import * as d3 from "d3";
 import {useRef, useEffect} from "react";
+import useSWR from 'swr';
+
 
 export default function BarChat(props){
-   const bg = useRef();
-   useEffect(() => void d3.select(bg.current)
+    const fetcher = (url) => fetch(url)
+          .then((response) => {
+             return response.json();
+           })
+           .then((data) => {
+              return JSON.stringify(data["data"])
+            }
+        );
+    const { data, error, isLoading } = useSWR(
+        props.URL,
+         fetcher)
+           
+ 
+    const bg = useRef();
+    useEffect(() => void d3.select(bg.current)
    .selectAll("rect")
-   .data(props.data)
+   .data(data)
    .enter()
    .append("rect")
    .attr("fill","black")
    .attr("x", (d, i) => i * 30)
    .attr("y", 0)
    .attr("width", 25)
-   
-   )
+   .attr("height", 50)
+   ) 
 
-
+    if (isLoading) return <p>Waiting for a second</p>
+    if (error) return <p>ERROR</p>
     return (
     <div>
         <svg 
