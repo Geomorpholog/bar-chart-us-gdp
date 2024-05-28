cff919dfdce7b243dd3c1005faef25f37e8c5540
Geomorpholog <Geomorpholog@rambler.ru>
2024-05-28 10:24:56 +0300

    remade

---
+++ public/barChat.js
@@ -0,0 +1,8 @@
+import dataSet from "./getData.js"
+
+d3.select("body")
+  .append("svg")
+                    
+    
+    
+   
---
+++ public/getData.js
@@ -0,0 +1,10 @@
+let dataSet
+let header = new Headers().append("Content-Type","texy/script")
+fetch(
+     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',
+     {header}
+     
+)
+     .then((response) =>{return response.json()})
+     .then((data) => dataSet = data["data"])
+export default dataSet
